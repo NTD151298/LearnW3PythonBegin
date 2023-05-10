779fe270aa41cb34a4e03f6c65ed80369b1fd7ad
16022305 <sieudenoo1512@gmail.com>
2023-05-10 14:30:19 +0700

    check thu xem la ta dang su dung ten 16022305 cua sieudenoo1512 co push duoc khong

---
+++ Git_add_commit.ps1
@@ -0,0 +1,2 @@
+git add .
+git commit -m "check thu xem la ta dang su dung ten 16022305 cua sieudenoo1512 co push duoc khong"
---
+++ User_add_to_Git.ps1
@@ -0,0 +1,5 @@
+#git config --global user.name "Tên người dùng của bạn"
+#git config --global user.email "Địa chỉ email của bạn"
+git config --global user.name "16022305"
+git config --global user.email "sieudenoo1512@gmail.com"
+

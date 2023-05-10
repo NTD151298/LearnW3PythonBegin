20f9cfd68c7c3ac045fbe1b84ec1e6243940676f
NTD1512 <duongtn1512@gmail.com>
2023-05-10 14:33:19 +0700

    check thu xem la ta dang su dung ten 16022305 cua sieudenoo1512 co push duoc khong

--- User_add_to_Git.ps1
+++ User_add_to_Git.ps1
@@ -1,5 +1,5 @@
 #git config --global user.name "Tên người dùng của bạn"
 #git config --global user.email "Địa chỉ email của bạn"
-git config --global user.name "16022305"
-git config --global user.email "sieudenoo1512@gmail.com"
+git config --global user.name "NTD1512"
+git config --global user.email "duongtn1512@gmail.com"
 

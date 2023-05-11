f9e8352763ee15f8c1a9687626ac517aaebfde9f
NTD1512 <duongtn1512@gmail.com>
2023-05-11 16:20:42 +0700

    Hoc duoc Python-Variables Python-Data-Types Python-Numbers tu 13h26 - 16h12 (02h36') code (01h22') fun(1h14')

--- Git_add_commit.ps1
+++ Git_add_commit.ps1
@@ -1,2 +1,2 @@
 git add .
-git commit -m "van tiep tuc ngay dau python thoi"
+git commit -m "Hoc duoc Python-Variables Python-Data-Types Python-Numbers tu 13h26 - 16h12 (02h36') code (01h22') fun(1h14')"

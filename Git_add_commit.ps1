517a723b72df929c4ab64c3a8d3044dc918d33cc
NTD1512 <duongtn1512@gmail.com>
2023-05-12 09:32:25 +0700

    Summary exerise at 09h31' 12-05-2023

--- Git_add_commit.ps1
+++ Git_add_commit.ps1
@@ -1,2 +1,2 @@
 git add .
-git commit -m "Summary at 22h34' Hoc 4 muc Python tren W3school chung ta code duoc 273 line va code trong 2h17'"
+git commit -m "Summary exerise at 09h31' 12-05-2023"
---
+++ Python_Learn_Day1/Exerise-Summary.py
@@ -0,0 +1,10 @@
+# Exerise-Summary
+print("Chung ta se luyen tap khoang 30' chia ra 10 phut cach mot hoat dong sau do nghi")
+a = "Chung ta dang luyen tap"
+print(a, "len(a) is :", len(a))
+print("Len tap chung vao cai lung nhi chung ta cu the\nVi don gian hon do la cu lam viet ra thoi")
+print("La mot ke sang tao\t nhung cai chu mau me\t va co the tap chung ma")
+x, x1, x2, x3 = int(34), int(43), float(93.43), complex(43j)
+print("x =", x, type(x), x1, type(x1), x2, type(x2), x3, type(x3))
+y, y1, y2, y3 = float(x), float(x1), int(x2), str(x3)
+print("y =", y, type(y), y1, type(y1), y2, type(y2), y3, type(y3))

b384f6574bcb4dff524078fc18aa2c17b3aacda6
NTD1512 <duongtn1512@gmail.com>
2023-05-21 02:15:03 +0700

    Dopamnie music  fast 20-05-2023

--- Git_add_commit.ps1
+++ Git_add_commit.ps1
@@ -1,2 +1,2 @@
 git add .
-git commit -m "Check viet dươc bao nhieu bang git coomit va add fix the wire new day try fast 20-05-2023"
+git commit -m "Dopamnie music  fast 20-05-2023"
--- Python_Learn_Day5_and_Beyone/Python-Lists.py
+++ Python_Learn_Day5_and_Beyone/Python-Lists.py
@@ -61,3 +61,84 @@ for x in list_duong1:
 print(list_duong1[-3:-1])
 list_duong1[-3:-1] = ['Binh', 'Duong']
 print(list_duong1[-3:-1])
+list_duong1[4:] = ['ngay', 'sinh', 'cua', 'toi', 15, 12, 1998]
+print(list_duong1, len(list_duong1))
+# Insert Items
+print("\nInsert Items")
+this_new_list = ['nguyen', 'hoang', 'duong']
+print(this_new_list)
+this_new_list.insert(3, 'Thanh')
+print(this_new_list)
+this_new_list.insert(0, 'Tran')
+print(this_new_list)
+this_new_list.insert(1, 'Dai')
+print(this_new_list)
+this_new_list[0] = 'Tran1'
+print(this_new_list)
+
+# Add List Items
+print("\nAdd List Items")
+# Append Items
+this_new_list.append('Trieuuu')
+print(this_new_list)
+
+# Remove Specified Item
+print("\nRemove Specified Item")
+print("this_new_list.remove('Trieuuu'): ")
+this_new_list.remove('Trieuuu')
+print(this_new_list)
+print("this_new_list.remove('duong')")
+this_new_list.remove('duong')
+print(this_new_list)
+print("this_new_list.pop(0): ")
+this_new_list.pop(0)
+print(this_new_list)
+print("del this_new_list[1]: ")
+del this_new_list[1]
+print(this_new_list)
+print("this_new_list.clear(): ")
+this_new_list.clear()
+print(this_new_list)
+print("del this_new_list: ")
+del this_new_list
+print("this_new_list = 12: ")
+this_new_list = 12
+print(this_new_list)
+
+# Loop Through a List
+print("\nLoop Through a List")
+list_duong2 = ['code    ', 'game    ', 'dopamine', 'live    ', 'job     ']
+print(list_duong2)
+i, j = 0, - len(list_duong2)
+for x in list_duong2:
+    print(x + "\t" + str(i) + "\t" + str(j))
+    i += 1
+    j += 1
+print("\nfor x in range(len(list_duong2)):")
+for x in range(len(list_duong2)):
+    print(list_duong2[x] + "\t" + str(x))
+a = range(1, 20, 3)
+print("\nfor x in a:")
+for x in a:
+    print(x)
+# Using a While Loop
+print("\nUsing a While Loop")
+i = 0
+j = - len(list_duong2)
+while i < len(list_duong2):
+    print(list_duong2[i] + "\t" + str(i) + "\t" + str(j))
+    i += 1
+    j += 1
+
+# List Comprehension
+print("List Comprehension")
+list_duong_new = []
+for x in list_duong2:
+    if "e" in x:
+        list_duong_new.append(x)
+print(list_duong_new)
+list_duong_new1 = []
+for x in list_duong2:
+    if "a" in x:
+        list_duong_new1.append(x)
+print(list_duong_new1)

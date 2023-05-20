e791794c37c475325eb06b5d7f176c5ed05442ba
NTD1512 <duongtn1512@gmail.com>
2023-05-20 07:30:03 +0700

    Talking and pointing to solve bro hold up and fix the wire new day try fast 20-05-2023

--- Git_add_commit.ps1
+++ Git_add_commit.ps1
@@ -1,2 +1,2 @@
 git add .
-git commit -m "Talk and point to solve bro hold up and fix the wire 03h07' 19-05-2023"
+git commit -m "Talking and pointing to solve bro hold up and fix the wire new day try fast 20-05-2023"
---
+++ Python_Learn_Day5_and_Beyone/Python-Lists.py
@@ -0,0 +1,39 @@
+
+# List Items - Data Types
+print("String list be like: ")
+my_list = ["Toi", 'Roi', 'cai', 'Gi']
+print(my_list, my_list[0], my_list[1], my_list[2], my_list[3])
+print("leng of list is: ", len(my_list))
+print("\nInterger list be like: ")
+list_Day7 = [20, 15, 12, 98, 19, 1, 2, 5, 3, 2, 5, 3, 34, 92, 43]
+print(list_Day7)
+print("leng of list is: ", len(list_Day7))
+print("\nBoolen list be like: ")
+list_boolen = [True, False, True, True, False]
+print(list_boolen)
+print("leng of list is: ", len(list_boolen))
+print("\nSum of 3 of the list: ")
+list_combie = list_Day7 + list_boolen + my_list
+print(list_combie)
+print("leng of list is: ", len(list_combie))
+
+This_list = list(("aple", "man", "good", "gun"))
+print("\nThis_list = list((\"aple\", \"man\", \"good\", \"gun\")): ")
+print(This_list)
+
+# There are four collection data types in the Python programming language:
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# Dictionary is a collection which is ordered** and changeable. No duplicate members.
+
+# Python - Access List Items
+print("\nPython - Access List Items")
+list_cout = ['Hom', 'nay', 'la', 'mot', 'cai']
+i = 0
+j = 0
+for x in list_cout:
+    print(x + "\t" + str(i) + "\t" + str(j))
+    i = i + 1
+    j = j - 1
+print("leng of list is: ", len(list_cout))
